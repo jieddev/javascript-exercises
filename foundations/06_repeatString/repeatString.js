7212f3d9f663f133bdb24718af27bb21191fc62d
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-08 10:13:13 +0800

    Completed repatString.js exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfTimesToRepeat) {
+  let attempts = 0;
+  let output = "";
 
+  if (numberOfTimesToRepeat < 0) {
+    return output += "ERROR";
+  }
+
+  while (attempts < numberOfTimesToRepeat ) {
+    output += string;
+    attempts++;
+  }
+  return output;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
