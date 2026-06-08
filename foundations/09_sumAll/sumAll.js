16434c45121c802396d8e19b182eb02a367ed21d
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-08 11:28:08 +0800

    Completed sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,33 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let large_number = 0;
+  let smallest_number = 0;
+
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
+
+  if (!Number.isInteger(a) || !Number.isInteger(b)){
+    return 'ERROR';
+  }
+
+  if (a > b) {
+    large_number = a;
+    smallest_number = b;
+  } else {
+    large_number = b;
+    smallest_number = a;
+  }
+
+  let arr = [];
+
+  for (let i = smallest_number; i <= large_number; i++){
+    arr.push(i);
+  }
+
+  return arr.reduce((accumulator, initialValue) => {
+    return accumulator + initialValue;
+  });
+
 
 };
 
