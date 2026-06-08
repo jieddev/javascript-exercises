997062582cf510c06703e26e7d838b9f69fb7159
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-08 10:42:24 +0800

    Completed reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  let reversedString = "";
+  let splittedString = string.split('');
+  for (let i = splittedString.length - 1; i >= 0; i--){
+    reversedString += splittedString[i];
+  }
 
+  return reversedString;
 };
 
 // Do not edit below this line
