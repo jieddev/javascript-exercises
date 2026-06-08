96b12a39db10d6656994267a3c5d18d523c56f04
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-08 11:55:38 +0800

    Complete tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  let decimalCelcius = (temperature - 32) * 5 / 9;
+
+  if (decimalCelcius == 0){
+    return 0;
+  } else {
+    return parseFloat(decimalCelcius.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  let fahrenheit = temperature * (9 / 5) + 32;
+  if ( fahrenheit == 0){
+    return 0;
+  } else {
+    return parseFloat(fahrenheit.toFixed(1));
+  }
 };
 
 // Do not edit below this line
