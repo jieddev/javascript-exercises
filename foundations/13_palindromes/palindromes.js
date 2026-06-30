1cd51d79e9cfb2b41723919856653154a262011d
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-30 21:24:32 +0800

    Complete activity 13 palindrome exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+  let simplifiedString = string.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~\s]/g, '').toLowerCase()
+  let reversed = simplifiedString.split("").reverse();
+  let reversedString = reversed.join("");
+  if (simplifiedString == reversedString){
+    return true;
+  } else {
+    return false;
+  }
+  console.log(string);
+  console.log(reversedString);
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
