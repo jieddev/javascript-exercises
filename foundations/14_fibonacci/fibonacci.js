ffcb79b528e64e5da75c2bab015cad4d7eb07c38
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-30 23:20:16 +0800

    Completed a bruteforce solution for the fibonacci sequence with O(n) time complexity

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+  let fibonnaciSequenceMaxIsN = [1, 1];
+
+  if (n < 0){
+    return "OOPS";
+  }
+
+  if (n == 0){
+    return 0;
+  };
+
+  for (index = 1; index < n; index++){
+    let sum = fibonnaciSequenceMaxIsN[index - 1] + fibonnaciSequenceMaxIsN[index];
+    fibonnaciSequenceMaxIsN.push(sum);
+  }
+
+  return fibonnaciSequenceMaxIsN[n - 1];
+
 
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
