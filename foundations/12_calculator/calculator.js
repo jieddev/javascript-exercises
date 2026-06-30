f3bcc50e99250bae97a0756f309725e03643e263
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-06-30 20:57:43 +0800

    Completed the number 12 calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, number) => {
+    return (total += number);
+  }, 0);
 };
 
-const multiply = function() {
+const multiply = function (array) {
+  return array.reduce((total, number) => {
+    return (total *= number);
+  }, 1);
+};
+console.log(multiply([1, 2, 3]))
 
+const power = function (base, exponent) {
+  let output = 1
+  for(index = 0; index < exponent; index++){
+    output *= base;
+  }
+  return output;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (number) {
+  let output = 1;
+  for(index = 1; index <= number; index++) {
+    output *= index;
+  }
+  return output;
 };
 
 // Do not edit below this line
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
